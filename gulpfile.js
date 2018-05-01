f77663a4e7263ad233a835f0ec4d0e73462b639b
xiaofan <1353815633@qq.com>
2018-05-01 19:19:15 +0800

    install babel

--- gulpfile.js
+++ gulpfile.js
@@ -11,25 +11,53 @@ var autoprefixer = require("gulp-autoprefixer");
 var eslint = require("gulp-eslint");
 var browserSync = require("browser-sync").create();
 var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+var babel = require("gulp-babel");
 
-gulp.task("lint", () => {
-  return gulp
-    .src(["**/*.js", "!node_modules/**"])
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
-});
+// gulp.task("lint", () => {
+//   return gulp
+//     .src(["**/*.js", "!node_modules/**"])
+//     .pipe(eslint())
+//     .pipe(eslint.format())
+//     .pipe(eslint.failAfterError());
+// });
+
+gulp.task(
+  "default",
+  ["copy-html", "copy-images", "styles", "lint", "scripts"],
+  function() {
+    gulp.watch("sass/**/*.scss", ["styles"]);
+    gulp.watch("js/**/*.js", ["lint"]);
+    gulp.watch("/index.html", ["copy-html"]);
+    gulp.watch("./dist/index.html").on("change", browserSync.reload);
+
+    browserSync.init({
+      server: "./dist"
+    });
+  }
+);
+
+gulp.task("dist", [
+  "copy-html",
+  "copy-images",
+  "styles",
+  "lint",
+  "scripts-dist"
+]);
 
 gulp.task("scripts", function() {
   gulp
     .src("js/**/*.js")
+    .pipe(babel())
     .pipe(concat("all.js"))
     .pipe(gulp.dest("dist/js"));
 });
+
 gulp.task("scripts-dist", function() {
   gulp
     .src("js/**/*.js")
     .pipe(concat("all.js"))
+    .pipe(uglify())
     .pipe(gulp.dest("dist/js"));
 });
 
